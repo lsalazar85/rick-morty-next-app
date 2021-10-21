0b33af831b0f2b4a8f96ab9eaa41bbffd9e84baf
luissalazar <l_salazar85@icloud.com>
2021-10-21 19:47:47 -0300

    Content scroll Y

--- styles/styles.tsx
+++ styles/styles.tsx
@@ -12,7 +12,17 @@ export const Content = styled.section`
   display: flex;
   flex-direction: column;
   padding: 1rem;
+  overflow-y: scroll;
   
+  ::-webkit-scrollbar {
+    width: 0.1rem;
+    background: #24281b;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #97ce4c;
+  }
+
   @media(min-width: 320px){
     width: 100%;
   }
